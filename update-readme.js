ae3e67b9b1b95f946371f65ee6f469cede9acfdf
Marcos Gonçalves <marcos.goncalves@avanade.com>
2024-01-05 15:53:49 -0300

    Feat: Currently working autoupdate

---
+++ update-readme.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const apiUrl = 'https://api.github.com/users/coppolaop/events/public';
+
+axios.get(apiUrl)
+    .then(response => {
+        const pushEvent = response.data.find(event => event.type === 'PushEvent');
+
+        if (!pushEvent) {
+            console.log('Nenhum evento do tipo "PushEvent" encontrado.');
+            process.exit(0);
+        }
+
+        const latestRepoName = pushEvent.repo.name;
+        const latestRepoUrl = pushEvent.repo.url;
+
+        const readmeContent = fs.readFileSync(readmePath, 'utf-8');
+
+        const updatedLine = `- 🔭 I’m currently working on [${latestRepoName}](${latestRepoUrl})`;
+
+        const updatedReadmeContent = readmeContent.replace(/- 🔭 I’m currently working on .*/, updatedLine);
+
+
+        fs.writeFileSync(readmePath, updatedReadmeContent, 'utf-8');
+    })
+    .catch(error => {
+        console.error('Erro ao obter dados da API:', error);
+        process.exit(1);
+    });
